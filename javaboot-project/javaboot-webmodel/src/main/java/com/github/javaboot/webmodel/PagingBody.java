bf80e2ee475a6dac253c7be56d958036bf62ebc1
codek_mk <codek_mk@163.com>
2019-11-04 12:35:25 +0800

    为 PagingBody 添加 setPageCountByTotalCountAndPageSize 方法

--- javaboot-project/javaboot-webmodel/src/main/java/com/github/javaboot/webmodel/PagingBody.java
+++ javaboot-project/javaboot-webmodel/src/main/java/com/github/javaboot/webmodel/PagingBody.java
@@ -11,8 +11,21 @@ import java.util.List;
  */
 @Data
 public class PagingBody<T> {
+  /** 记录总数 */
   private long totalCount;
+  /** 当前页的记录集合 */
   private List<T> items;
-
+  /** 总的分页总数 */
   private long pageCount;
+
+  /**
+   * 通过 totalCount 和 pageSize 计算 pageCount
+   *
+   * @param totalCount
+   * @param pageSize
+   */
+  public void setPageCountByTotalCountAndPageSize(long totalCount, long pageSize) {
+    this.totalCount = totalCount;
+    this.pageCount = ((totalCount - 1) / pageSize) + 1;
+  }
 }
--- javaboot-project/javaboot-webmodel/src/test/java/com/github/javaboot/webmodel/PagingBodyTest.java
+++ javaboot-project/javaboot-webmodel/src/test/java/com/github/javaboot/webmodel/PagingBodyTest.java
@@ -12,4 +12,15 @@ class PagingBodyTest {
     assertThat(pb.getTotalCount()).isEqualTo(0);
     assertThat(pb.getItems()).isNull();
   }
+
+  @Test
+  void should_return_pageCount_by_totalCount_and_pageSize() {
+    final long pageSize = 10;
+    final long totalCount = 100;
+    PagingBody<String> pb = new PagingBody<>();
+    pb.setPageCountByTotalCountAndPageSize(totalCount, pageSize);
+
+    assertThat(pb.getTotalCount()).isEqualTo(totalCount);
+    assertThat(pb.getPageCount()).isEqualTo((totalCount - 1) / pageSize + 1);
+  }
 }
