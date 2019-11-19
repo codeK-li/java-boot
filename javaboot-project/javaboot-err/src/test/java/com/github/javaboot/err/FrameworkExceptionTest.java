5611a51b33e9ca9fad529166e3cec6a823e65cc8
codek_mk <codek_mk@163.com>
2019-11-19 11:38:38 +0800

    通过 Framework 构造各种 Exception

--- javaboot-project/javaboot-err/src/test/java/com/github/javaboot/err/FrameworkExceptionTest.java
+++ javaboot-project/javaboot-err/src/test/java/com/github/javaboot/err/FrameworkExceptionTest.java
@@ -7,21 +7,73 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FrameworkExceptionTest {
   @Test
   void should_return_the_same_ErrorMessage() {
-    ErrorMessage em = new ErrorMessage();
-    FrameworkException ae = new FrameworkException(em);
+    {
+      ErrorMessage em = new ErrorMessage();
+      FrameworkException ae = FrameworkException.system(em);
 
-    assertThat(ae.getErrorMessage()).isEqualTo(em);
-    assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.EXCEPTION_LEVEL);
+      assertThat(ae.getErrorMessage()).isEqualTo(em);
+      assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.SYSTEM_LEVEL);
+    }
+    {
+      ErrorMessage em = new ErrorMessage();
+      FrameworkException ae = FrameworkException.framework(em);
+
+      assertThat(ae.getErrorMessage()).isEqualTo(em);
+      assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.FRAMEWORK_LEVEL);
+    }
+    {
+      ErrorMessage em = new ErrorMessage();
+      FrameworkException ae = FrameworkException.application(em);
+
+      assertThat(ae.getErrorMessage()).isEqualTo(em);
+      assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.APPLICATION_LEVEL);
+    }
+    {
+      ErrorMessage em = new ErrorMessage();
+      FrameworkException ae = FrameworkException.business(em);
+
+      assertThat(ae.getErrorMessage()).isEqualTo(em);
+      assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.BUSINESS_LEVEL);
+    }
   }
 
   @Test
   void should_return_the_same_ErrorMessage_and_Throwable() {
-    ErrorMessage em = new ErrorMessage();
-    Throwable t = new Exception();
-    FrameworkException ae = new FrameworkException(em, t);
+    {
+      ErrorMessage em = new ErrorMessage();
+      Throwable t = new Exception();
+      FrameworkException ae = FrameworkException.system(em, t);
 
-    assertThat(ae.getErrorMessage()).isEqualTo(em);
-    assertThat(ae.getCause()).isEqualTo(t);
-    assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.EXCEPTION_LEVEL);
+      assertThat(ae.getErrorMessage()).isEqualTo(em);
+      assertThat(ae.getCause()).isEqualTo(t);
+      assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.SYSTEM_LEVEL);
+    }
+    {
+      ErrorMessage em = new ErrorMessage();
+      Throwable t = new Exception();
+      FrameworkException ae = FrameworkException.framework(em, t);
+
+      assertThat(ae.getErrorMessage()).isEqualTo(em);
+      assertThat(ae.getCause()).isEqualTo(t);
+      assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.FRAMEWORK_LEVEL);
+    }
+    {
+      ErrorMessage em = new ErrorMessage();
+      Throwable t = new Exception();
+      FrameworkException ae = FrameworkException.application(em, t);
+
+      assertThat(ae.getErrorMessage()).isEqualTo(em);
+      assertThat(ae.getCause()).isEqualTo(t);
+      assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.APPLICATION_LEVEL);
+    }
+    {
+      ErrorMessage em = new ErrorMessage();
+      Throwable t = new Exception();
+      FrameworkException ae = FrameworkException.business(em, t);
+
+      assertThat(ae.getErrorMessage()).isEqualTo(em);
+      assertThat(ae.getCause()).isEqualTo(t);
+      assertThat(ae.getExceptionLevel()).isEqualTo(FrameworkException.BUSINESS_LEVEL);
+    }
   }
 }
