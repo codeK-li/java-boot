92a733e28e39cb672e8c469323ba079c76c8be64
codek_mk <codek_mk@163.com>
2019-11-25 16:32:11 +0800

    删除不需要的测试代码

--- javaboot-project/springboot-starters/javaboot-webmvc-helper-starter/src/test/java/com/github/javaboot/webhelper/WebMvcConfigurationTest.java
+++ javaboot-project/springboot-starters/javaboot-webmvc-helper-starter/src/test/java/com/github/javaboot/webhelper/WebMvcConfigurationTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +15,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(
-    classes = {
-      WebMvcConfiguration.class,
-      WebMvcConfigurationTest.class,
-      WebMvcConfigurationTest.C.class
-    })
+@SpringBootTest(classes = {WebMvcConfiguration.class, WebMvcConfigurationTest.C.class})
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration
 @AutoConfigureMockMvc
 class WebMvcConfigurationTest {
   @Autowired private MockMvc mvc;
