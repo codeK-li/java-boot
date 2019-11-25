9052194d98a2feaecefb78370ac704fd8afa9e52
codek_mk <codek_mk@163.com>
2019-11-25 16:28:30 +0800

    添加 WebMvc 的启动类

---
+++ javaboot-project/springboot-starters/javaboot-webmvc-helper-starter/src/main/java/com/github/javaboot/webhelper/WebMvcConfiguration.java
@@ -0,0 +1,35 @@
+package com.github.javaboot.webhelper;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * WebMvc 模块的启动项
+ *
+ * @author liweichao11
+ * @since 2018/3/16
+ */
+@ConditionalOnClass(DispatcherServlet.class)
+@ConditionalOnMissingBean(RequestContextListener.class)
+@Configuration
+@ComponentScan
+class WebMvcConfiguration {
+
+  @Order(Ordered.HIGHEST_PRECEDENCE)
+  @Bean
+  public RequestContextListener requestContextListener() {
+    return new RequestContextListener();
+  }
+
+  @Bean
+  public WebMvcHelper webHelper() {
+    return new WebMvcHelperImpl();
+  }
+}
